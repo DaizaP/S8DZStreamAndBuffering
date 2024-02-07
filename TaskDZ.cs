cad752e830534b2dffca253d8727baafa36539db
Daiza <salkodg@gmail.com>
2024-02-08 02:24:16 +0500

    Добавил поиск нескольких подходящих файлов

--- TaskDZ.cs
+++ TaskDZ.cs
@@ -1,18 +1,25 @@
-﻿namespace S8DZStreamAndBuffering
+﻿using System.Text;
+
+namespace S8DZStreamAndBuffering
 {
     class TaskDZ
     {
         public static void TaskHomerork(string format, string textInFile)
         {
             //Пробегаемся по всем дискам
-            string[] pathArr = Environment.GetLogicalDrives();
-            foreach (string path in pathArr)
+            string[] DriveArr = Environment.GetLogicalDrives();
+            List<string> resPath = new List<string>();
+            foreach (string path in DriveArr)
             {
-                string res = FindFile(format, path, textInFile);
-                if (res != "NULL") { Console.WriteLine(res); }
+                FindFile(format, path, textInFile, resPath);
             }
+            foreach (string path in resPath)
+            {
+                if (path != "NULL") { Console.WriteLine(path); }
+            }
+            
         }
-        static string FindFile(string format, string dir, string textInFile)
+        static string FindFile(string format, string dir, string textInFile, List<string> stringsPath)
         {
             //Игнорируем директории к которым нет доступа
             var options = new EnumerationOptions()
@@ -23,20 +30,23 @@
             {
                 if (Path.GetExtension(name) == format)
                 {
-                    using (StreamReader streamReader = new StreamReader(name))
+                    //Ищет все файлы с текстом игнорируя исключения, но и выполняется дольше
+                    try
                     {
-                        string str = "Найдено";
-                        if (streamReader.ReadToEnd().Equals(textInFile))
+                        using (StreamReader streamReader = new StreamReader(name))
                         {
-                            return name;
+                            if (streamReader.ReadToEnd().Equals(textInFile))
+                            {
+                                stringsPath.Add(name);
+                            }
                         }
-
                     }
+                    catch { }
                 }
             }
             foreach (string directory in Directory.GetDirectories(dir, "*", options))
             {
-                string temp = FindFile(format, directory, textInFile);
+                string temp = FindFile(format, directory, textInFile, stringsPath);
                 if (temp != "NULL")
                 {
                     return temp;
